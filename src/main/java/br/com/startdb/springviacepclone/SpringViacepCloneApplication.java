ecf26c1e36cc5255214917e8f16ecb401013a4f5
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-10-22 20:43:51 -0300

    initial project setup

---
+++ src/main/java/br/com/startdb/springviacepclone/SpringViacepCloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.startdb.springviacepclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringViacepCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringViacepCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/startdb/springviacepclone/SpringViacepCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.startdb.springviacepclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringViacepCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
