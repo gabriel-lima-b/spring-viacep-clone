e71ca2018ea4d1971db7a98a9d6cdf08a4c96cb0
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2021-10-25 14:25:12 -0300

    Adding @AutoWired in CepController

--- src/main/java/br/com/startdb/springviacepclone/CepController.java
+++ src/main/java/br/com/startdb/springviacepclone/CepController.java
@@ -1,5 +1,6 @@
 package br.com.startdb.springviacepclone;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,26 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(exposedHeaders = "errors,content-type")
 public class CepController {
 
+	@Autowired
 	private CepRepository cepRepository;
 
 	@RequestMapping(value = "/{cep}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<Cep> getCep(@PathVariable("cep") String cep) {
-		// save a few ceps
-		cepRepository.save(new Cep("90420020", "Rua Professor Álvaro Alvim", "", "Rio Branco", "Porto Alegre", "RS",
-				"4314902", "", "51", "8801"));
-		cepRepository.save(new Cep("91530080", "Rua Oswaldo Pereira de Freitas", "", "Partenon", "Porto Alegre", "RS",
-				"4314902", "", "51", "8801"));
-		cepRepository.save(new Cep("90050100", "Rua General Lima e Silva", "até 868 - lado par", "Centro Histórico",
-				"Porto Alegre", "RS", "4314902", "", "51", "8801"));
-		cepRepository.save(new Cep("91530034", "Rua Professor Cristiano Fischer", "de 1900 ao fim - lado par",
-				"Partenon", "Porto Alegre", "RS", "4314902", "", "51", "8801"));
-		cepRepository.save(
-				new Cep("91530010", "Rua Albion", "", "Partenon", "Porto Alegre", "RS", "4314902", "", "51", "8801"));
-
 		Cep cepObj = cepRepository.findByCep(cep);
 		if (cep == null) {
 			return new ResponseEntity<Cep>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<Cep>(cepObj, HttpStatus.OK);
+	}
+	@RequestMapping(value = "/all", method = RequestMethod.GET, produces = "application/json")
+	public Iterable<Cep> getAllCep() {
+		return cepRepository.findAll();
 	}
 }
